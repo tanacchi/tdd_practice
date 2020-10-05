ca21442446d82e5c5460d3778297d7600178182d
tanacchi <q111026d@mail.kyutech.jp>
2020-10-05 23:23:25 +0900

    Add test script for lisp interpreter

---
+++ lisp_interpreter/tests/test_lisp_interpreter.py
@@ -0,0 +1,13 @@
+import sys
+sys.path.append("..")
+
+import unittest
+
+
+class TestFizzBuzz(unittest.TestCase):
+    def test_run(self):
+        self.assertTrue(True)
+
+
+if __name__ == '__main__':
+    unittest.main()
