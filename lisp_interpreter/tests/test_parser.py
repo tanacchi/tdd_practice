9373cf35d7424c0161383f499f6343949fd83ce8
tanacchi <q111026d@mail.kyutech.jp>
2020-10-10 22:50:52 +0900

    Add test script for parser

---
+++ lisp_interpreter/tests/test_parser.py
@@ -0,0 +1,21 @@
+import sys
+
+sys.path.append("..")
+
+import unittest
+
+from src.repl import parse
+
+
+class TestParser(unittest.TestCase):
+    def test_run(self):
+        self.assertTrue(True)
+
+    def test_simple_string(self):
+        actual = parse("ahi")
+        expect = "ahi"
+        self.assertEqual(actual, expect)
+
+
+if __name__ == '__main__':
+    unittest.main()
